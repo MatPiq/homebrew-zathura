3ffa10a57faadaec2f6688e3bf8d8bd6e0b1fc0a
Matias Piqueras <matiaspiqueras.1994@gmail.com>
2024-12-05 20:36:30 +0100

    remove titlebar

--- Formula/zathura.rb
+++ Formula/zathura.rb
@@ -37,6 +37,40 @@ class Zathura < Formula
     end
   end
 
+    def install
+    # Set Homebrew prefix
+    ENV['PREFIX'] = prefix
+    # Add the pkgconfig for girara to the PKG_CONFIG_PATH
+    # TODO: Find out why it is not added correctly for Linux
+    ENV['PKG_CONFIG_PATH'] = "#{ENV['PKG_CONFIG_PATH']}:#{Formula['girara'].prefix}/lib/x86_64-linux-gnu/pkgconfig"
+
+    inreplace "meson.build" do |s|
+      s.gsub! "subdir('doc')", ""
+    end
+    
+    # Adding in the titlebar modifications
+    
+    inreplace "zathura/zathura.c" do |s|
+      s.gsub! "GdkWindow* window = gtk_widget_get_window(zathura->ui.session->gtk.view);", "
+  GdkWindow* window = gtk_widget_get_window(zathura->ui.session->gtk.view);
+  GtkWidget* topLevelWidget = gtk_widget_get_toplevel(zathura->ui.session->gtk.view); // TopLevel is (in zathura) always a GtkWindow, so we just check to see if it is NULL to prevent crashing.
+  if (topLevelWidget == NULL) {
+    return;
+  }
+  gtk_window_set_titlebar(GTK_WINDOW(topLevelWidget), gtk_header_bar_new()); // Casting GtkWindow to the GtkWidget to fit the function and creating a new (empty) titlebar."
+    end
+
+    inreplace "data/zathura.css_t" do |s|
+      s.gsub! "\#@session@ .indexmode:selected {", "
+window {
+  border-radius: 10px;
+}
+\#@session@ .statusbar {
+  border-radius: 0px 0px 10px 10px; /* Rounding only the bottom corners to correlate with the window. */
+}
+\#@session@ .indexmode:selected {"
+    end
+
   test do
     system "true" # TODO
   end
